82df94d7d51aef024843e4506875011bc5147504
Khanh Nguyen <khanhngn.dev@gmail.com>
2024-04-25 01:01:16 +0700

    Add postman collection

--- Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb62527eab09e400c7785a517a41c30d18c353c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b80d44d44ca64fac6169215ba3664203633c22c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
